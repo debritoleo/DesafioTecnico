bb4215c61e92b4a4457c0912cd192c90fad3c710
Leonardo de brito <leo_guty11@hotmail.com>
2020-07-02 00:24:13 -0300

    Desenvolvido testes de calculo de juros. (Business)

---
+++ DesafioTecnico/DesafioTecnico.Calculos.Test/Business/ServicoCalculoJurosTests.cs
@@ -0,0 +1,34 @@
+﻿using DesafioTecnico.Calculos.Business.Interfaces;
+using DesafioTecnico.Calculos.Business.Services;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DesafioTecnico.Calculos.Test.Business
+{
+    public class ServicoCalculoJurosTests
+    {
+        private Mock<IServicoTaxaJuros> _servicoTaxa;
+        private IServicoCalculoJuros _servicoCalculo;
+
+        public ServicoCalculoJurosTests()
+        {
+            _servicoTaxa = new Mock<IServicoTaxaJuros>();
+            _servicoCalculo = new ServicoCalculoJuros(_servicoTaxa.Object);
+        }
+
+        [Theory]
+        [InlineData(0.01, 1000, 2, 1020.10)]
+        [InlineData(1, 2000, 2, 8000)]
+        [InlineData(0.01, 100, 5, 105.1010050100)]
+
+        public void ServicoCalculoJuros_ChamarCalcularJuros_RetornarSucesso(decimal taxaJuros, decimal valorInicial, int meses, decimal valorFinal)
+        {
+            _servicoTaxa.Setup(x => x.ObterTaxaJuros()).Returns(taxaJuros);
+
+            var valorCalculado = _servicoCalculo.CalcularJuros(valorInicial, meses);
+
+            valorCalculado.Should().Be(valorFinal);
+        }
+    }
+}
