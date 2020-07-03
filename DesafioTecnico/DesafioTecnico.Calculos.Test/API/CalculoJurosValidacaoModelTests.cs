26f01a515e4f9f92b99bb2ca45f95678c23ba0ff
Leonardo de brito <leo_guty11@hotmail.com>
2020-07-02 22:00:53 -0300

    Inclusao de teste de model de validação de parametros de entrada do calculo de juros.

---
+++ DesafioTecnico/DesafioTecnico.Calculos.Test/API/CalculoJurosValidacaoModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using DesafioTecnico.Calculos.API.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace DesafioTecnico.Calculos.Test.API
+{
+    public class CalculoJurosValidacaoModelTests
+    {
+        private readonly CalculoJurosValidacaoModel _modelValidacao;
+        public CalculoJurosValidacaoModelTests()
+        {
+            _modelValidacao = new CalculoJurosValidacaoModel();
+        }
+
+        [Theory]
+        [InlineData(null, "6", "Informe o valor inicial")]
+        [InlineData("", "6", "Informe o valor inicial")]
+        [InlineData("teste", "6", "Informe um número válido para valor inicial")]
+        [InlineData("-10", "6", "Informe um número válido para valor inicial")]
+        [InlineData("c1", "6", "Informe um número válido para valor inicial")]
+        [InlineData("100", null, "Informe o número de meses")]
+        [InlineData("100", "", "Informe o número de meses")]
+        [InlineData("100", "22.8", "Meses deve ser um número válido")]
+        [InlineData("100", "-1", "Meses deve ser um número válido")]
+        public void CalculoJurosValidacaoModel_Validar_RetornarFalha(string valorInicial, string meses, string erro)
+        {
+            var result = _modelValidacao.Validar(valorInicial, meses);
+
+            result.Should().BeFalse();
+            _modelValidacao.Erros.FirstOrDefault().Should().Be(erro);
+        }
+    }
+}
--- DesafioTecnico/DesafioTecnico.Calculos.Test/API/CalculosJurosTests.cs
+++ DesafioTecnico/DesafioTecnico.Calculos.Test/API/CalculosJurosTests.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace DesafioTecnico.Calculos.Test.API
 {
     public class CalculosJurosTests
     {
+        public CalculosJurosTests()
+        {
+                
+        }
+
+        //public async Task Obter_CalcularJuros_RetornarSucesso(string valorInicial, string meses, string resultadoEsperado)
+        //{
+        //    var path = $"/calculajuros?valorinicial={valorInicial}&meses={meses}";
+        //    var result = await _client.GetAsync(path);
+        //    result.EnsureSuccessStatusCode();
+        //    var resultString = await result.Content.ReadAsStringAsync();
+        //    resultString.Should().Be(resultadoEsperado);
+        //}
     }
 }
